477da7eb32630bf8936f998f8f53187ac3aa2e13
hermann-web <hermannagossou6@gmail.com>
2024-02-24 19:15:09 +0100

    add run-prettier

---
+++ run-prettier.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Function to check if a command is installed globally and provide installation instructions if it's not found
+check_command() {
+    local command_name=$1
+    local install_command=$2
+
+    if ! command -v "$command_name" &> /dev/null; then
+        echo "$command_name is not installed."
+        echo "To install $command_name, run: $install_command"
+        return 1
+    fi
+
+    return 0
+}
+
+check_command "prettier" "npm install prettier --global" || return 1
+prettier *.html --write
+prettier ./css/*.css --write
