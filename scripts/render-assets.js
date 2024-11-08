912c3adea57884d9c29bfb0c968719a73f6dcdd8
hermann-web <hermannagossou6@gmail.com>
2024-11-08 16:34:32 +0100

    use jrecc template for winter school 2025

---
+++ scripts/render-assets.js
@@ -0,0 +1,12 @@
+'use strict';
+const fs = require('fs');
+const upath = require('upath');
+const sh = require('shelljs');
+
+module.exports = function renderAssets() {
+    const sourcePath = upath.resolve(upath.dirname(__filename), '../src/assets');
+    const destPath = upath.resolve(upath.dirname(__filename), '../dist/.');
+    
+    sh.cp(upath.resolve(sourcePath, 'favicon.ico'), destPath);
+    sh.cp('-R', sourcePath, destPath)
+};
