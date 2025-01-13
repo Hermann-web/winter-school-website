35d5ee2db2529aad95dac34808031aca9d745131
hermann-web <69398651+Hermann-web@users.noreply.github.com>
2025-01-13 16:14:45 +0100

    updates pre build

---
+++ jj.sh
@@ -0,0 +1,7 @@
+curl  -H 'accept: application/json'   -H 'telephone: 00212635706898'   -H 'password: stink123'   -d '' -i -X 'POST'   'http://localhost:3001/api/restaurants/mineasrestau'
+
+
+# curl  -H 'accept: application/json'   -H 'telephone: 00212635706898'   -H 'password: stink123'   -d '' -i -X 'POST' 'http://localhost:3001/api/restaurants/mineasrestau/?isActive=true&isOnline=false'
+
+
+
